b82427105ea047480a670fd678e774c1568c1643
Jordan Abreu <jabreu610@gmail.com>
2023-01-19 16:50:15 -0500

    ✨ implements problem 1

--- problem_1/explanation_1.md
+++ problem_1/explanation_1.md
@@ -1,5 +1,7 @@
 # Explanation 1
 
 ## Rationale
+For the "Finding the Square Root of an Integer" problem, the solution for every valid input lies within the range of all natural numbers between zero and the input, I leveraged binary search to find the floored square root of a provided integer. 
 
 ## Runtime Analysis
+The runtime complexity of this solution is `O(log n)` where `n` is the range of natural numbers between zero and the input value, its space complexity is `O(1)` as memory utilization remains constant regardless of input in the worst case.
--- problem_1/problem_1.py
+++ problem_1/problem_1.py
@@ -1,2 +1,47 @@
+def sqrt(num: int) -> int:
+    """Returns the floored square root of a provided integer."""
+    if num < 0:
+        raise ValueError(
+            """Math domain error. Unable to compute the square root of 
+            negative numbers, complex numbers are not supported by this implementation""")
+    elif num <=1:
+        return num
+
+    upper_bound = num // 2
+    lower_bound = 0
+
+    while lower_bound <= upper_bound:
+        mid = (lower_bound + upper_bound ) // 2
+        square = mid ** 2
+
+        if square > num:
+            upper_bound = mid - 1
+        elif square < num:
+            lower_bound = mid + 1
+        else:
+            return mid
+    
+    return upper_bound
+
+
+
 if __name__ == "__main__":
-    pass
+    print(sqrt(25) == 5)
+    # True
+    print(sqrt(2**52) == 67108864)
+    # True
+    print(sqrt(2**52 + 1) == 67108864)
+    # True
+    print(sqrt(3) == 1)
+    # True
+    print(sqrt(2) == 1)
+    # True
+    print(sqrt(1) == 1)
+    # True
+    print(sqrt(0) == 0)
+    # True
+    try:
+        sqrt(-25)
+    except ValueError as e:
+        print(e)
+    # Math domain error. Unable to compute the square root of negative numbers, complex numbers are not supported by this implementation
