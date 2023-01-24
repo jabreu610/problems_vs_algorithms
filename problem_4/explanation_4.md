ef46dc78229f9129e9104fa035fc0663746004e7
Jordan Abreu <jabreu610@gmail.com>
2023-01-24 13:20:57 -0500

    ✨ implements problem 4

--- problem_4/explanation_4.md
+++ problem_4/explanation_4.md
@@ -1,5 +1,7 @@
 # Explanation 4
 
 ## Rationale
+For the "Dutch National Flag Problem", I iterate through the `input_list` exactly once as specified by the problem definition. Through this iteration I build up an sorted answer list.
 
 ## Runtime Analysis
+The runtime complexity of this solution is `O(n)` and its space complexity is `O(n)` where n is the length of the unsorted input array. Runtime scales linearly with the input array's length as we iterate through the input once to build our result. Memory utilization also scales linearly with input length as we build the result list as a seperate array.
--- problem_4/problem_4.py
+++ problem_4/problem_4.py
@@ -1,2 +1,44 @@
+from random import randrange
+
+def sort_012(input_list):
+    """
+    Given an input array consisting on only 0, 1, and 2, sort the array in a single traversal.
+    """
+    last_zero_index = 0
+    ans = []
+    for val in input_list:
+        if val == 0:
+            ans = [val] + ans
+            last_zero_index += 1
+        if val == 2:
+            ans += [val]
+        if val == 1:
+            ans = ans[:last_zero_index] + [val] + ans[last_zero_index:]
+
+    return ans
+
+
 if __name__ == "__main__":
-    pass
+
+    print(sort_012([0, 0, 2, 2, 2, 1, 1, 1, 2, 0, 2])
+          == sorted([0, 0, 2, 2, 2, 1, 1, 1, 2, 0, 2]))
+    # True
+    print(sort_012([2, 1, 2, 0, 0, 2, 1, 0, 1, 0, 0, 2, 2, 2, 1, 2, 0, 0, 0, 2, 1, 0, 2, 0, 0, 1]) == sorted(
+        [2, 1, 2, 0, 0, 2, 1, 0, 1, 0, 0, 2, 2, 2, 1, 2, 0, 0, 0, 2, 1, 0, 2, 0, 0, 1]))
+    # True
+    print(sort_012([0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2]) == sorted(
+        [0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2]))
+    # True
+    print(sort_012([2, 1, 0]) == sorted([2, 1, 0]))
+    # True
+    print(sort_012([2, 1]) == sorted([2, 1]))
+    # True
+    print(sort_012([1, 0]) == sorted([1, 0]))
+    # True
+    print(sort_012([2, 0]) == sorted([2, 0]))
+    # True
+    print(sort_012([]) == sorted([]))
+    # True
+    long_input = [randrange(3) for _ in range(2 ** 8)]
+    print(sort_012(long_input) == sorted(long_input))
+    # True
